66f1f110b537405bcdc63795e3f76eceeac413cc
Luke <dewald0725@gmail.com>
2024-08-14 15:53:48 +0200

    tf img classification init

---
+++ tf-img-classification.py
@@ -0,0 +1,128 @@
+# https://www.tensorflow.org/tutorials/images/classification
+
+import PIL.Image
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras 
+from tensorflow.keras import layers # type: ignore
+from tensorflow.keras.models import Sequential # type: ignore
+
+import pathlib
+
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file('flower_photos.tar', origin=dataset_url, extract = True)
+data_dir = pathlib.Path(data_dir).with_suffix('')
+
+image_count = len(list(data_dir.glob('*/*.jpg')))
+
+# roses = list(data_dir.glob('roses/*'))
+# PIL.Image.open(str(roses[0])).show()
+# PIL.Image.open(str(roses[1])).show()
+
+# tulips = list(data_dir.glob('tulips/*'))
+# PIL.Image.open(str(tulips[0])).show()
+# PIL.Image.open(str(tulips[1])).show()
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split = 0.2,
+    subset = "training",
+    seed = 123,
+    image_size = (img_height, img_width),
+    batch_size= batch_size
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split = 0.2,
+    subset = "validation",
+    seed = 123,
+    image_size = (img_height, img_width),
+    batch_size= batch_size
+)
+
+class_names = train_ds.class_names
+
+
+
+plt.figure(figsize = (10, 10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot( 3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        
+# plt.show()
+
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size = AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size = AUTOTUNE)
+
+normalization_layer = layers.Rescaling(1./255, input_shape = (img_height, img_width, 3))
+
+# normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+# image_batch, labels_batch = next(iter(normalized_ds))
+# first_image = image_batch[0]
+
+# print(np.min(first_image), np.max(first_image))
+
+num_classes = len(class_names)
+
+model = Sequential([
+    normalization_layer,
+    layers.Conv2D(16, 3, padding='same', activation = 'relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation = 'relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation = 'relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
+])
+
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+
+model.summary()
+
+epochs = 10
+history = model.fit(
+    train_ds,
+    validation_data = val_ds,
+    epochs = epochs
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2 , 1)
+plt.plot(epochs_range, acc, label = 'Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc = 'lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot( 1, 2, 2)
+plt.plot(epochs_range, loss, label = 'Training Loss')
+plt.plot(epochs_range, val_loss, label = 'Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
