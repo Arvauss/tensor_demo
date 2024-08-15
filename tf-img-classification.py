5a7c821786a69cad36b70ee26a1fbd72dc979ffa
Luke <dewald0725@gmail.com>
2024-08-15 10:51:25 +0200

    final img classifc

--- tf-img-classification.py
+++ tf-img-classification.py
@@ -53,20 +53,20 @@ class_names = train_ds.class_names
 
 
 
-plt.figure(figsize = (10, 10))
-for images, labels in train_ds.take(1):
-    for i in range(9):
-        ax = plt.subplot( 3, 3, i + 1)
-        plt.imshow(images[i].numpy().astype("uint8"))
-        plt.title(class_names[labels[i]])
+# plt.figure(figsize = (10, 10))
+# for images, labels in train_ds.take(1):
+#     for i in range(9):
+#         ax = plt.subplot( 3, 3, i + 1)
+#         plt.imshow(images[i].numpy().astype("uint8"))
+#         plt.title(class_names[labels[i]])
         
-# plt.show()
+# # plt.show()
 
 AUTOTUNE = tf.data.AUTOTUNE
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size = AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size = AUTOTUNE)
 
-normalization_layer = layers.Rescaling(1./255, input_shape = (img_height, img_width, 3))
+normalization_layer = layers.Rescaling(1./255)
 
 # normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 # image_batch, labels_batch = next(iter(normalized_ds))
@@ -76,7 +76,17 @@ normalization_layer = layers.Rescaling(1./255, input_shape = (img_height, img_wi
 
 num_classes = len(class_names)
 
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal",
+                          input_shape = (img_height, img_width, 3)),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.1),
+    ]
+)
+
 model = Sequential([
+    data_augmentation,
     normalization_layer,
     layers.Conv2D(16, 3, padding='same', activation = 'relu'),
     layers.MaxPooling2D(),
@@ -95,9 +105,9 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 
-model.summary()
+# model.summary()
 
-epochs = 10
+epochs = 15
 history = model.fit(
     train_ds,
     validation_data = val_ds,
@@ -126,3 +136,32 @@ plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
 
+
+
+# plt.figure(figsize=(10, 10))
+# for images, _ in train_ds.take(1):
+#     for i in range(9):
+#         augmented_images = data_augmentation(images)
+#         ax = plt.subplot(3, 3, i+ 1)
+#         plt.imshow(augmented_images[0].numpy().astype('uint8'))
+#         plt.axis('off')
+# plt.show()
+
+sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
+sunflower_path = tf.keras.utils.get_file('Red_sunflower', origin=sunflower_url)
+
+img = tf.keras.utils.load_img(
+    sunflower_path, target_size = (img_height, img_width)
+    
+)
+
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
